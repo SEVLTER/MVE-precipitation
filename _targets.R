86a134d0d94a6b9ac984c76386362f086204953f
Kenji Hayashi <kenjithayashi@gmail.com>
2026-09-21 14:59:49 -0700

    Prepare input and output directories

--- _targets.R
+++ _targets.R
@@ -3,12 +3,16 @@
 # Load packages required to define the pipeline
 library(targets)
 library(tarchetypes)
+library(fs)
 
 # Set target options
 tar_option_set()
 
 # Source R scripts
 tar_source()
+
+# Prepare directories
+dir_create("output")
 
 # Define the target list
 list()
