e131a5290e7150aa7f6a5d2b22de373e20dd91ca
Kenji Hayashi <kenjithayashi@gmail.com>
2026-09-21 14:27:21 -0700

    Initialize targets pipeline

---
+++ _targets.R
@@ -0,0 +1,14 @@
+# {targets} pipeline for constructing precipitation histories in MVE plots
+
+# Load packages required to define the pipeline
+library(targets)
+library(tarchetypes)
+
+# Set target options
+tar_option_set()
+
+# Source R scripts
+tar_source()
+
+# Define the target list
+list()
